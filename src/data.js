886e0b7dab56ce2d9db780050bdcad5a02e5ef62
unknown <viczik16@gmail.com>
2023-04-14 14:35:26 +0100

    new job

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 export default [
+
     {
         id : 1,
         title: "Travel Jornal",
@@ -24,6 +25,24 @@ export default [
         projectUrl: "https://victoromuya.github.io/dice-game/",
         imageUrl: "https://github.com/victoromuya/dice-game/blob/master/shot.JPG?raw=true",
         completed: 0
+    },
+
+    {
+        id : 4,
+        title: "Gmail Clone",
+        description: "Built with Reactjs, Redux and Firebase",
+        projectUrl: "https://clone-bb7d2.web.app/",
+        imageUrl: "https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png",
+        completed: 1
+    },
+
+    {
+        id : 5,
+        title: "LinkedIn Clone",
+        description: "Built with Reactjs, Redux and Firebase",
+        projectUrl: "https://linkedin-clone-d1e54.web.app",
+        imageUrl: "https://cdn.iconscout.com/icon/free/png-256/linkedin-47-189764.png?f=avif&w=128",
+        completed: 1
     },
 
 ]
